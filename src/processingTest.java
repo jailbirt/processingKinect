2a4f8521ed3db23cd97207a919c53e82ffcfc3d2
Javier Ailbirt <jailbirt@gmail.com>
2012-05-25 19:59:03 -0300

    first commit

---
+++ src/processingTest.java
@@ -0,0 +1,15 @@
+import processing.core.*;
+public class processingTest extends PApplet {
+
+	  public void setup() {
+	    size(200,200);
+	    background(0);
+	  }
+
+	  public void draw() {
+	    stroke(255);
+	    if (mousePressed) {
+	      line(mouseX,mouseY,pmouseX,pmouseY);
+	    }
+	  }
+}
